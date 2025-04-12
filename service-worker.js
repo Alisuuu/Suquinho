a60eee3ef92a588dd137d0af766e3481a96fd2e8
Alisuuu <132578134+Alisuuu@users.noreply.github.com>
2025-04-12 11:31:19 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'suquinho-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/favicon.ico',
+  '/nave.png',
+  '/p.png',
+  '/planeta.webp',
+  // arquivos das pastas
+  '/Catalogo1/index.html',
+  '/Hyper/index.html',
+  '/Yt/index.html',
+  '/game/index.html',
+  '/links/index.html',
+  '/not2/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
