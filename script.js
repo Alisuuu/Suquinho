fd6fd7e4acf651730b915a4f6bac49f5627d051b
Alisuuu <132578134+Alisuuu@users.noreply.github.com>
2025-04-12 11:31:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -62,3 +62,10 @@ document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('orientationchange', checkOrientation);
     checkOrientation(); // Verifica a orientação inicial
 });
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log('Service Worker registrado:', reg))
+      .catch(err => console.error('Erro ao registrar Service Worker:', err));
+  });
+}
