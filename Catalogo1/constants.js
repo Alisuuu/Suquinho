a2a78ad8190511515701d78fac4afca5d0ee901a
Alisuuu <132578134+Alisuuu@users.noreply.github.com>
2025-05-24 03:39:44 -0300

    Delete Catalogo1/constants.js

--- Catalogo1/constants.js
+++
@@ -1,4 +0,0 @@
-const TMDB_API_KEY = '5e5da432e96174227b25086fe8637985';
-const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
-const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
-const GEMINI_API_KEY = ""; // Replace with your actual Gemini API Key
