3f37d042e26bccbd056f6004279ae0fb0795719a
Alisuuu <132578134+Alisuuu@users.noreply.github.com>
2025-05-24 04:25:40 -0300

    Update script1.js

--- Catalogo1/script1.js
+++ Catalogo1/script1.js
@@ -6,5 +6,5 @@ App.Constants = {
     TMDB_API_KEY: '5e5da432e96174227b25086fe8637985',
     TMDB_BASE_URL: 'https://api.themoviedb.org/3',
     IMAGE_BASE_URL: 'https://image.tmdb.org/t/p/w500',
-    GEMINI_API_KEY: "" // Replace with your actual Gemini API Key
+    GEMINI_API_KEY: "AIzaSyBKek4_SgKcWBunUrmV48yvPoqdR4sr-Pk" // Replace with your actual Gemini API Key
 };
